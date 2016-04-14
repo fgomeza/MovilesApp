99a750c679581a58bbe00035108a65f66462d8f4
Brenda Ruiz <brendaruizm@hotmail.com>
2016-04-13 22:25:05 -0600

    Tabs

---
+++ app/src/main/java/com/example/francisco/movilesapp/TabFragment2.java
@@ -0,0 +1,20 @@
+package com.example.francisco.movilesapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by brendaruiz on 4/13/16.
+ */
+public class TabFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+
+    }
+
+}
